267f102b7e57336c7830f429c9ffa015b501484c
PLG1010 <pierrelouis.pilougdu17@gmail.com>
2025-06-26 14:36:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.all('/proxy', async (req, res) => {
   try {
     const url = req.query.url;
-    if (!url) {
-      return res.status(400).send('Paramètre "url" requis !');
-    }
-    const axiosConfig = {
+    if (!url) return res.status(400).send('URL requise !');
+    const response = await axios({
       method: req.method,
-      url: url,
+      url,
       headers: { ...req.headers, host: undefined },
       data: req.body,
-    };
-    const response = await axios(axiosConfig);
+    });
     res.status(response.status).send(response.data);
   } catch (e) {
     res.status(500).send(e.toString());
